df2f3b91dc456237fe9a0fe081c75301edf9ec06
Andrei <andreykapas@gmail.com>
2025-04-07 11:49:40 +0200

    Fix logo path for Vercel

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <footer className={'mt-auto flex items-center justify-between border-t border-white/10 h-16 px-3 sm:px-9' +
       'text-xs text-white/25'}>
       <small className={'text-xs'}>
-        &copy; 2025 APrystayka. All rights reserved.
+        &copy; 2025 Andrei Prystayka. All rights reserved.
       </small>
       <ul className={'flex gap-x-3 sm:gap-x-8'}>
         {
