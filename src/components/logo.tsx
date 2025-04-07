3492112f20298d6fbe0a2ca9b7fadbd2fd42eb94
Andrei <andreykapas@gmail.com>
2025-04-07 11:46:34 +0200

    Fix logo path for Vercel

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,16 +1,9 @@
-'use client';
-
 import Image from 'next/image'
-import { usePathname } from 'next/navigation'
 
 const Logo = () => {
-  const pathname = usePathname()
-  const isGitHub = pathname.startsWith('/evento')
-  const prefix = isGitHub ? '/evento' : ''
-
   return (
     <Image
-      src={`${prefix}/evento/evento.png`}
+      src="/evento.png"
       alt="EVENTO logo"
       width={53}
       height={12}
